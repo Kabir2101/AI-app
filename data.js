c7f3c9ac2d1058309928a4e516eeb48fe3437052
Kabir2101 <65112841+Kabir2101@users.noreply.github.com>
2021-09-26 15:47:58 +0530

    data.js

---
+++ data.js
@@ -0,0 +1,58 @@
+let lists = [
+    {
+        question: "what is your name",
+        answer: "my name is Max.",
+    },
+    {
+        question: "what's your name",
+        answer: "my name is Max.",
+    },
+    {
+        question: "what do you do",
+        answer: "I help developers write better code",
+    },
+    {
+        question: "introduce yourself",
+        answer: "my name is Max, I help developers write better code. Made by Kabir Ayaz Raj, I make people's life easier.",
+    },
+    {
+        question: "introduce your developer",
+        answer: "his name is Kabir Ayaz Raj. As a web developer, he is aiming to make the world a better place to live. But he strictly doesn't make useless video games as he wishes that no one in the world plays video games.",
+    },
+    {
+        question: "what is used for building websites",
+        answer: "HTML, CSS AND JavaScript. The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. JavaScript often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.",
+    },
+    {
+        question: "what is good for deploying websites",
+        answer: "my developer says that azure is bad. He instead uses netlify for deploying his websites.",
+    },
+    {
+        question: "what is python popular for",
+        answer: "Python is known to be good for developing AI.",
+    },
+    {
+        question: "what programming languages are you made of",
+        answer: "I am made of HTML, CSS and JavaScript.",
+    },
+    {
+        question: "do you know everything",
+        answer: "No, But I am becoming better day by day.",
+    },
+    {
+        question: "give me info about microsoft",
+        answer: "Microsoft Corporation is an American multinational technology corporation which produces computer software, consumer electronics, personal computers, and related services. Its best known software products are the Microsoft Windows line of operating systems, the Microsoft Office suite, and the Internet Explorer and Edge web browsers. Its flagship hardware products are the Xbox video game consoles and the Microsoft Surface lineup of touchscreen personal computers. Microsoft ranked No. 21 in the 2020 Fortune 500 rankings of the largest United States corporations by total revenue; it was the world's largest software maker by revenue as of 2016. It is considered one of the Big Five companies in the U.S. information technology industry, along with Google, Apple, Amazon, and Facebook.",
+    },
+    {
+        question: "who is the founder of microsoft",
+        answer: "Bill Gates is the founder of Microsoft.",
+    },
+    {
+        question: "give me info about google",
+        answer: "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware. It is considered one of the Big Five companies in the American information technology industry, along with Amazon, Facebook, Apple, and Microsoft.",
+    },
+    {
+        question: "who are the founders of google",
+        answer: "Larry Page and Sergey Brin are the founders of google",
+    }
+]
